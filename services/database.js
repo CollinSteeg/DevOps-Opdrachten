f07671f7f2d26e15368f09fe545b28d59e8191ab
CollinSteeg <167075925+CollinSteeg@users.noreply.github.com>
2026-02-11 21:47:39 +0100

    test

--- services/database.js
+++ services/database.js
@@ -4,6 +4,8 @@ const uri = process.env.MONGO_URL;
 const client = new MongoClient(uri);
 const db = client.db(process.env.DB_NAME);
 
+console.log(process.env.DB_NAME + " " + process.env.MONGO_URL);
+
 module.exports = {
     db: db,
     client: client
